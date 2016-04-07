4f536e98ef6685eb8f5a63feb64790aed0b8c967
0xFireball <0xFireball@outlook.com>
2016-04-06 19:52:14 -0700

    update ignore, add icon, add template

---
+++ VSAddin/WebFormsTemplate/WebFormsApplication.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebFormsTemplate
+{
+    public class WebFormsApplication
+    {
+        public static void Main(string[] args)
+        {
+            //Add Foundation CSS
+            var foundationCssLink = new LinkElement()
+            {
+                Relation = "stylesheet",
+                HREF = "foundation.min.css",
+            };
+            //Run the application
+            new WebApplication(new CSSUITheme(CSSFramework.Foundation6)).Run(new MainForm(), "formhost");
+        }
+    }
+}
