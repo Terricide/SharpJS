e010720311e0faa5005f84eec91e9bee92ee0fad
0xFireball <0xFireball@outlook.com>
2016-04-06 19:52:22 -0700

    add mainform template

---
+++ VSAddin/WebFormsTemplate/MainForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ExaPhaser.WebForms;
+
+namespace WebFormsTemplate
+{
+    /// <summary>
+	/// Main WebForm
+	/// </summary>
+	public class MainForm : WebForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            
+        }
+    }
+}
