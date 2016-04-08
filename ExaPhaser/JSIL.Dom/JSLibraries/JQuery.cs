832d69d59376cabed3adfc5cb01296e2abffa386
0xFireball <0xFireball@outlook.com>
2016-04-08 11:25:52 -0700

    add empty jQuery wrapper

---
+++ ExaPhaser/JSIL.Dom/JSLibraries/JQuery.cs
@@ -0,0 +1,10 @@
+﻿namespace JSIL.Dom.JSLibraries
+{
+    public static class JQuery
+    {
+        static JQuery()
+        {
+            Verbatim.Expression("var jq = jQuery.noConflict(true)");
+        }
+    }
+}
