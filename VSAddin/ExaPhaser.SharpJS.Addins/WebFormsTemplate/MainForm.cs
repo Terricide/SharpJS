59b7d2ab76c79774ef7e87f7b44d8d8f2c1a6ca4
0xFireball <0xFireball@outlook.com>
2016-04-11 23:39:40 -0700

    update addin

--- VSAddin/ExaPhaser.SharpJS.Addins/WebFormsTemplate/MainForm.cs
+++ VSAddin/ExaPhaser.SharpJS.Addins/WebFormsTemplate/MainForm.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using ExaPhaser.WebForms;
 using ExaPhaser.WebForms.Controls;
+using JSIL.Dom;
 using JSIL.Dom.Styles;
 
 namespace WebFormsTemplate
@@ -10,64 +11,36 @@ namespace WebFormsTemplate
 	/// </summary>
 	public class MainForm : WebForm
     {
-        #region Private Fields
-
-        private Button button1;
-        private PreformattedTextBlock outputBox1;
-        private TextBlock textBlock1;
-        private TextBox textBox1;
-        private TextBlock titleBlock;
-
-        #endregion Private Fields
-
-        #region Public Constructors
-
         public MainForm()
         {
-        }
-
-        #endregion Public Constructors
-
-        #region Public Methods
-
-        public void addNameToBox(string name)
-        {
-            textBox1.Text = "";
-            outputBox1.Text += name + "\n";
-        }
-
-        public override void PerformLayout()
-        {
-            base.PerformLayout();
-            titleBlock = new TextBlock();
-            this.Controls.Add(titleBlock);
-            textBlock1 = new TextBlock();
-            this.Controls.Add(textBlock1);
-            textBox1 = new TextBox();
-            textBox1.EnterPressed += (object sender, EventArgs e) => addNameToBox(textBox1.Text);
-            this.Controls.Add(textBox1);
-            button1 = new Button();
-            button1.Click += (object sender, EventArgs e) => addNameToBox(textBox1.Text);
-            this.Controls.Add(button1);
-            outputBox1 = new PreformattedTextBlock();
-            this.Controls.Add(outputBox1);
-        }
-        public override void UpdateContent()
-        {
-            base.UpdateContent();
-            titleBlock.Text = "ExaPhaser.WebForms Demo";
-            titleBlock.FontStyle = new FontStyle()
+            Controls = new Layout()
             {
-                FontWeight = FontWeight.Bold,
-                FontSize = 24,
+                new TextBlock()
+                {
+                    Text = "ExaPhaser.WebForms Demo",
+                    TextAlign = TextAlign.Center,
+                    FontStyle = new FontStyle()
+                    {
+                        FontSize = 24,
+                        FontWeight = FontWeight.Bold,
+                    }
+                },
+                new TextBlock()
+                {
+                    Text = "A random TextBox is below",
+                },
+                new TextBox()
+                {
+                    Text = "Some text",
+                },
+                new Button()
+                {
+                    Text = "OK",
+                    CommandParameter = null,
+                    Command = new DelegateCommand(()=>JSLibrary.Alert("You clicked the button")),
+                }
             };
-            titleBlock.TextAlign = TextAlign.Center;
-            textBlock1.Text = "Enter your name:";
-            button1.Text = "OK";
-            outputBox1.Text = "";
-            Console.WriteLine("It Works!");
         }
-
-        #endregion Public Methods
+        
     }
 }
