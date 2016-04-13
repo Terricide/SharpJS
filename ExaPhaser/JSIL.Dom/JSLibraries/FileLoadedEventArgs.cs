249a81760e27154333128f7f54b918be0a73daa2
0xFireball <0xFireball@outlook.com>
2016-04-12 22:34:13 -0700

    add fileloadedeventargs

---
+++ ExaPhaser/JSIL.Dom/JSLibraries/FileLoadedEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSIL.Dom.JSLibraries
+{
+    public class FileLoadedEventArgs : EventArgs
+    {
+        public object Target;
+    }
+
+    public delegate void FileLoadedEventHandler(object sender, FileLoadedEventArgs e);
+}
--- ExaPhaser/JSIL.Dom/JSLibraries/FileReader.cs
+++ ExaPhaser/JSIL.Dom/JSLibraries/FileReader.cs
@@ -1,7 +1,29 @@
-﻿namespace JSIL.Dom.JSLibraries
+﻿using System;
+
+namespace JSIL.Dom.JSLibraries
 {
     public class FileReader
     {
+        public FileReader()
+        {
+            object _fileReaderHandle;
+            bool fileReaderSupported = Builtins.IsTruthy(Verbatim.Expression("'FileReader' in window"));
+            if (!fileReaderSupported)
+            {
+                throw new InvalidOperationException("The browser does not support the HTML5 FileReader API!");
+            }
+            _fileReaderHandle = Verbatim.Expression("new FileReader()");
+        }
 
+        private void OnLoad(object sender, FileLoadedEventArgs e)
+        {
+            FileLoadedEventHandler handler = Load;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        public event FileLoadedEventHandler Load;
     }
 }
