23a8701c1b961d98a1858b66fb4fce8588d2c976
0xFireball <0xFireball@outlook.com>
2016-04-13 22:30:08 -0700

    add textarea

---
+++ ExaPhaser/Frameworks/ExaPhaser.WebForms/Controls/TextArea.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using JSIL.Dom.Elements;
+
+namespace ExaPhaser.WebForms.Controls
+{
+    public class TextArea : TextControl
+    {
+        #region Public Constructors
+
+        public TextArea() : base()
+        {
+            InternalElement = new TextAreaElement();
+            InternalElement.Change += OnTextChanged;
+            InternalJQElement.EnterKeyPressed += OnEnterPressed;
+            InternalJQElement.Focus += OnFocus;
+            PerformLayout();
+        }
+
+        #endregion Public Constructors
+
+        #region Public Events
+
+        public event EventHandler EnterPressed;
+
+        public event EventHandler Focus;
+
+        public event EventHandler TextChanged;
+
+        #endregion Public Events
+
+        #region Public Properties
+
+        public string Text
+        {
+            get { return GetText(); } //We are using GetText because the text
+            set { SetText(value); }
+        }
+
+        public int Rows
+        {
+            get { return int.Parse(InternalElement["rows"]); }
+            set { InternalElement["rows"] = value.ToString(); }
+        }
+
+        public int Columns
+        {
+            get { return int.Parse(InternalElement["cols"]); }
+            set { InternalElement["cols"] = value.ToString(); }
+        }
+
+        #endregion Public Properties
+
+        #region Private Methods
+
+        private string GetText()
+        {
+            return InternalElement["value"];
+        }
+
+        private void OnEnterPressed(object sender, EventArgs e)
+        {
+            EventHandler handler = EnterPressed;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        private void OnFocus(object sender, EventArgs e)
+        {
+            EventHandler handler = Focus;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        private void OnTextChanged(object sender, EventArgs e)
+        {
+            EventHandler handler = TextChanged;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        private void SetText(string value)
+        {
+            InternalElement["value"] = value;
+        }
+
+        #endregion Private Methods
+    }
+}
---
+++ ExaPhaser/JSIL.Dom/Elements/TextAreaElement.cs
@@ -0,0 +1,21 @@
+﻿namespace JSIL.Dom.Elements
+{
+    public class TextAreaElement : Element
+    {
+        public TextAreaElement() : base("textarea")
+        {
+        }
+
+        public int Rows
+        {
+            get { return int.Parse(this["rows"]); }
+            set { this["rows"] = value.ToString(); }
+        }
+
+        public int Columns
+        {
+            get { return int.Parse(this["cols"]); }
+            set { this["cols"] = value.ToString(); }
+        }
+    }
+}
