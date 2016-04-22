29e90242b07f768a332af73beb59389d5ce29438
0xFireball <0xFireball@outlook.com>
2016-04-21 18:36:16 -0700

    update nuget to v22, update VSIX info, switch to PolyUI CSS

--- VSAddin/ExaPhaser.SharpJS.Addins/WebFormsTemplate/WebFormsApplication.cs
+++ VSAddin/ExaPhaser.SharpJS.Addins/WebFormsTemplate/WebFormsApplication.cs
@@ -9,38 +9,8 @@ namespace WebFormsTemplate
     {
         public static void Main(string[] args)
         {
-            #region Apply Styles to page
-
-            //Add Foundation CSS
-            var foundationCssLink = new LinkElement()
-            {
-                Relation = "stylesheet",
-                HREF = "foundation.min.css",
-            };
-            var formhost = Document.GetElementById("formhost"); //Get the form host div
-            var row = new DivElement() //Create Row
-            {
-                Class = "row",
-            };
-            var columns = new DivElement() //Create columns
-            {
-                Class = "large-12 columns",
-            };
-            //Create a panel/callout for the form
-            var panel = new DivElement()
-            {
-                Class = "callout",
-                Id = "formhostcontainer",
-            };
-            columns.AppendChild(panel);
-            row.AppendChild(columns);
-            formhost.AppendChild(row);
-            Document.Body.AppendChild(foundationCssLink);
-
-            #endregion Apply Styles to page
-
             //Run the application
-            new WebApplication(new CSSUITheme(CSSFramework.Foundation6)).Run(new MainForm(), "formhostcontainer");
+            new WebApplication(new CSSUITheme(CSSFramework.PolyUI)).Run(new MainForm(), "webform-container");
         }
     }
 }
