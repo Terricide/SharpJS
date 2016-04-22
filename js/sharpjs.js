13fb7848f31a3612250faa42fd5751407677eab7
0xFireball <0xFireball@outlook.com>
2016-04-21 19:27:42 -0700

    add license requirement to GH page

---
+++ js/sharpjs.js
@@ -0,0 +1,28 @@
+var reject = $("#license-reject");
+var accept = $("#license-acceptbtn");
+var licensing = $("#licensing-agreement");
+var finaldownload = $("#finaldownload");
+finaldownload.hide();
+
+
+reject.click(function () {
+    licensing.fadeOut(500, function () {
+        licensing.empty();
+        licensing.show();
+        licensing.html('<div class="text-center"><h2>We\'re sorry we couldn\'t help you today.<h2> <h5>Maybe you should go check out some more projects.</h5> <a href="https://github.com/exaphaser" class="btn btn-default btn-lg">Take me away</a></div>');
+    });
+});
+
+
+accept.click(function () {
+    licensing.fadeOut(500, function () {
+        licensing.empty();
+        licensing.show();
+        licensing.html('<div class="text-center"><h2>Great! Thanks for using SharpJS!</h2></div>');
+        finaldownload.fadeIn(500, function () {
+            $('html, body').animate({
+                scrollTop: finaldownload.offset().top
+            }, 400);
+        });
+    });
+});
