ad0bd6159334a13bbb686921437170f4e05696ca
0xFireball <0xFireball@outlook.com>
2016-04-25 17:00:18 -0700

    add documentation

---
+++ docs/dom/Html/default/data/Navigation.js
@@ -0,0 +1,286 @@
+
+
+var sharpDox = sharpDox || {};
+
+sharpDox.navigationData = [
+			
+
+	{
+		id: 'SharpJS.Dom',
+		text: 'SharpJS.Dom',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#!namespace/SharpJS.Dom' },
+		children: [
+			
+ 
+
+		{
+			id: 'SharpJS.Dom.Document',
+			text: 'Document',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.Document' }
+        },	
+		{
+			id: 'SharpJS.Dom.Element',
+			text: 'Element',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.Element' }
+        },	
+		{
+			id: 'SharpJS.Dom.IntervalDispatcher',
+			text: 'IntervalDispatcher',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.IntervalDispatcher' }
+        },	
+		{
+			id: 'SharpJS.Dom.JSConsole',
+			text: 'JSConsole',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.JSConsole' }
+        },	
+		{
+			id: 'SharpJS.Dom.JSLibrary',
+			text: 'JSLibrary',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.JSLibrary' }
+        },	
+		{
+			id: 'SharpJS.Dom.RgbStyle',
+			text: 'RgbStyle',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.RgbStyle' }
+        },	
+		{
+			id: 'SharpJS.Dom.StringStyle',
+			text: 'StringStyle',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.StringStyle' }
+        },	
+		{
+			id: 'SharpJS.Dom.Style',
+			text: 'Style',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.Style' }
+        },	
+		{
+			id: 'SharpJS.Dom.StyleCollection',
+			text: 'StyleCollection',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.StyleCollection' }
+        },	
+
+
+		]
+	},
+	{
+		id: 'SharpJS.Dom.Attributes',
+		text: 'SharpJS.Dom.Attributes',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#!namespace/SharpJS.Dom.Attributes' },
+		children: [
+			
+ 
+
+		{
+			id: 'SharpJS.Dom.Attributes.InputType',
+			text: 'InputType',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.Attributes.InputType' }
+        },	
+
+
+		]
+	},
+	{
+		id: 'SharpJS.Dom.Elements',
+		text: 'SharpJS.Dom.Elements',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#!namespace/SharpJS.Dom.Elements' },
+		children: [
+			
+ 
+
+		{
+			id: 'SharpJS.Dom.Elements.AnchorElement',
+			text: 'AnchorElement',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.Elements.AnchorElement' }
+        },	
+		{
+			id: 'SharpJS.Dom.Elements.ButtonElement',
+			text: 'ButtonElement',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.Elements.ButtonElement' }
+        },	
+		{
+			id: 'SharpJS.Dom.Elements.CanvasElement',
+			text: 'CanvasElement',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.Elements.CanvasElement' }
+        },	
+		{
+			id: 'SharpJS.Dom.Elements.DivElement',
+			text: 'DivElement',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.Elements.DivElement' }
+        },	
+		{
+			id: 'SharpJS.Dom.Elements.FileUploadElement',
+			text: 'FileUploadElement',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.Elements.FileUploadElement' }
+        },	
+		{
+			id: 'SharpJS.Dom.Elements.FormElement',
+			text: 'FormElement',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.Elements.FormElement' }
+        },	
+		{
+			id: 'SharpJS.Dom.Elements.HeaderElement',
+			text: 'HeaderElement',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.Elements.HeaderElement' }
+        },	
+		{
+			id: 'SharpJS.Dom.Elements.IFrameElement',
+			text: 'IFrameElement',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.Elements.IFrameElement' }
+        },	
+		{
+			id: 'SharpJS.Dom.Elements.ImageElement',
+			text: 'ImageElement',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.Elements.ImageElement' }
+        },	
+		{
+			id: 'SharpJS.Dom.Elements.InputElement',
+			text: 'InputElement',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.Elements.InputElement' }
+        },	
+		{
+			id: 'SharpJS.Dom.Elements.LinkElement',
+			text: 'LinkElement',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.Elements.LinkElement' }
+        },	
+		{
+			id: 'SharpJS.Dom.Elements.ParagraphElement',
+			text: 'ParagraphElement',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.Elements.ParagraphElement' }
+        },	
+		{
+			id: 'SharpJS.Dom.Elements.PreformattedElement',
+			text: 'PreformattedElement',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.Elements.PreformattedElement' }
+        },	
+		{
+			id: 'SharpJS.Dom.Elements.ScriptElement',
+			text: 'ScriptElement',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.Elements.ScriptElement' }
+        },	
+		{
+			id: 'SharpJS.Dom.Elements.TextAreaElement',
+			text: 'TextAreaElement',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.Elements.TextAreaElement' }
+        },	
+
+
+		]
+	},
+	{
+		id: 'SharpJS.Dom.JSLibraries',
+		text: 'SharpJS.Dom.JSLibraries',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#!namespace/SharpJS.Dom.JSLibraries' },
+		children: [
+			
+ 
+
+		{
+			id: 'SharpJS.Dom.JSLibraries.FileLoadedEventArgs',
+			text: 'FileLoadedEventArgs',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.JSLibraries.FileLoadedEventArgs' }
+        },	
+		{
+			id: 'SharpJS.Dom.JSLibraries.FileReader',
+			text: 'FileReader',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.JSLibraries.FileReader' }
+        },	
+		{
+			id: 'SharpJS.Dom.JSLibraries.JQElement',
+			text: 'JQElement',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.JSLibraries.JQElement' }
+        },	
+		{
+			id: 'SharpJS.Dom.JSLibraries.JQuery',
+			text: 'JQuery',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.JSLibraries.JQuery' }
+        },	
+		{
+			id: 'SharpJS.Dom.JSLibraries.JQueryObject',
+			text: 'JQueryObject',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.JSLibraries.JQueryObject' }
+        },	
+
+
+		]
+	},
+	{
+		id: 'SharpJS.Dom.JSLibraries.JQElements',
+		text: 'SharpJS.Dom.JSLibraries.JQElements',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#!namespace/SharpJS.Dom.JSLibraries.JQElements' },
+		children: [
+			
+ 
+
+		{
+			id: 'SharpJS.Dom.JSLibraries.JQElements.JQDivElement',
+			text: 'JQDivElement',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.JSLibraries.JQElements.JQDivElement' }
+        },	
+
+
+		]
+	},
+	{
+		id: 'SharpJS.Dom.Styles',
+		text: 'SharpJS.Dom.Styles',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#!namespace/SharpJS.Dom.Styles' },
+		children: [
+			
+ 
+
+		{
+			id: 'SharpJS.Dom.Styles.FontWeight',
+			text: 'FontWeight',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.Styles.FontWeight' }
+        },	
+		{
+			id: 'SharpJS.Dom.Styles.TextAlign',
+			text: 'TextAlign',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/SharpJS.Dom.Styles.TextAlign' }
+        },	
+
+
+		]
+	},
+
+
+	];
---
+++ docs/webforms/Html/default/data/Navigation.js
@@ -0,0 +1,218 @@
+
+
+var sharpDox = sharpDox || {};
+
+sharpDox.navigationData = [
+			
+
+	{
+		id: 'ExaPhaser.WebForms',
+		text: 'ExaPhaser.WebForms',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#!namespace/ExaPhaser.WebForms' },
+		children: [
+			
+ 
+
+		{
+			id: 'ExaPhaser.WebForms.Control',
+			text: 'Control',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/ExaPhaser.WebForms.Control' }
+        },	
+		{
+			id: 'ExaPhaser.WebForms.ControlCollection',
+			text: 'ControlCollection',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/ExaPhaser.WebForms.ControlCollection' }
+        },	
+		{
+			id: 'ExaPhaser.WebForms.DelegateCommand',
+			text: 'DelegateCommand',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/ExaPhaser.WebForms.DelegateCommand' }
+        },	
+		{
+			id: 'ExaPhaser.WebForms.FontStyle',
+			text: 'FontStyle',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/ExaPhaser.WebForms.FontStyle' }
+        },	
+		{
+			id: 'ExaPhaser.WebForms.ICommand',
+			text: 'ICommand',
+			icon: './assets/icons/interface_public.png',
+			a_attr: { href : '#!type/ExaPhaser.WebForms.ICommand' }
+        },	
+		{
+			id: 'ExaPhaser.WebForms.Layout',
+			text: 'Layout',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/ExaPhaser.WebForms.Layout' }
+        },	
+		{
+			id: 'ExaPhaser.WebForms.PositioningType',
+			text: 'PositioningType',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/ExaPhaser.WebForms.PositioningType' }
+        },	
+		{
+			id: 'ExaPhaser.WebForms.WebApplication',
+			text: 'WebApplication',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/ExaPhaser.WebForms.WebApplication' }
+        },	
+		{
+			id: 'ExaPhaser.WebForms.WebForm',
+			text: 'WebForm',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/ExaPhaser.WebForms.WebForm' }
+        },	
+
+
+		]
+	},
+	{
+		id: 'ExaPhaser.WebForms.Controls',
+		text: 'ExaPhaser.WebForms.Controls',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#!namespace/ExaPhaser.WebForms.Controls' },
+		children: [
+			
+ 
+
+		{
+			id: 'ExaPhaser.WebForms.Controls.Button',
+			text: 'Button',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/ExaPhaser.WebForms.Controls.Button' }
+        },	
+		{
+			id: 'ExaPhaser.WebForms.Controls.ElementGroup',
+			text: 'ElementGroup',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/ExaPhaser.WebForms.Controls.ElementGroup' }
+        },	
+		{
+			id: 'ExaPhaser.WebForms.Controls.HtmlControl',
+			text: 'HtmlControl',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/ExaPhaser.WebForms.Controls.HtmlControl' }
+        },	
+		{
+			id: 'ExaPhaser.WebForms.Controls.ImageView',
+			text: 'ImageView',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/ExaPhaser.WebForms.Controls.ImageView' }
+        },	
+		{
+			id: 'ExaPhaser.WebForms.Controls.InputType',
+			text: 'InputType',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/ExaPhaser.WebForms.Controls.InputType' }
+        },	
+		{
+			id: 'ExaPhaser.WebForms.Controls.LinkLabel',
+			text: 'LinkLabel',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/ExaPhaser.WebForms.Controls.LinkLabel' }
+        },	
+		{
+			id: 'ExaPhaser.WebForms.Controls.Panel',
+			text: 'Panel',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/ExaPhaser.WebForms.Controls.Panel' }
+        },	
+		{
+			id: 'ExaPhaser.WebForms.Controls.PreformattedTextBlock',
+			text: 'PreformattedTextBlock',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/ExaPhaser.WebForms.Controls.PreformattedTextBlock' }
+        },	
+		{
+			id: 'ExaPhaser.WebForms.Controls.TextArea',
+			text: 'TextArea',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/ExaPhaser.WebForms.Controls.TextArea' }
+        },	
+		{
+			id: 'ExaPhaser.WebForms.Controls.TextBlock',
+			text: 'TextBlock',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/ExaPhaser.WebForms.Controls.TextBlock' }
+        },	
+		{
+			id: 'ExaPhaser.WebForms.Controls.TextBox',
+			text: 'TextBox',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/ExaPhaser.WebForms.Controls.TextBox' }
+        },	
+		{
+			id: 'ExaPhaser.WebForms.Controls.TextControl',
+			text: 'TextControl',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/ExaPhaser.WebForms.Controls.TextControl' }
+        },	
+		{
+			id: 'ExaPhaser.WebForms.Controls.WebView',
+			text: 'WebView',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/ExaPhaser.WebForms.Controls.WebView' }
+        },	
+
+
+		]
+	},
+	{
+		id: 'ExaPhaser.WebForms.Drawing',
+		text: 'ExaPhaser.WebForms.Drawing',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#!namespace/ExaPhaser.WebForms.Drawing' },
+		children: [
+			
+ 
+
+		{
+			id: 'ExaPhaser.WebForms.Drawing.Point',
+			text: 'Point',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/ExaPhaser.WebForms.Drawing.Point' }
+        },	
+		{
+			id: 'ExaPhaser.WebForms.Drawing.RelativePoint',
+			text: 'RelativePoint',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/ExaPhaser.WebForms.Drawing.RelativePoint' }
+        },	
+
+
+		]
+	},
+	{
+		id: 'ExaPhaser.WebForms.Themes',
+		text: 'ExaPhaser.WebForms.Themes',
+		icon: './assets/icons/namespace_public.png',
+		a_attr: { href : '#!namespace/ExaPhaser.WebForms.Themes' },
+		children: [
+			
+ 
+
+		{
+			id: 'ExaPhaser.WebForms.Themes.CSSFramework',
+			text: 'CSSFramework',
+			icon: './assets/icons/enum_public.png',
+			a_attr: { href : '#!type/ExaPhaser.WebForms.Themes.CSSFramework' }
+        },	
+		{
+			id: 'ExaPhaser.WebForms.Themes.CSSUITheme',
+			text: 'CSSUITheme',
+			icon: './assets/icons/class_public.png',
+			a_attr: { href : '#!type/ExaPhaser.WebForms.Themes.CSSUITheme' }
+        },	
+
+
+		]
+	},
+
+
+	];
