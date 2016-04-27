76fae37e2248732dd79b51047869fc68d60c0269
0xFireball <0xFireball@outlook.com>
2016-04-27 15:12:08 -0700

    add form porting project abstraction layer stub

---
+++ ExaPhaser/Extensions/ExaPhaser.WebForms.Extensions.WinFormsCompat/Windows/Forms/Application.cs
@@ -0,0 +1,21 @@
+﻿using ExaPhaser.WebForms;
+using ExaPhaser.WebForms.Themes;
+
+namespace System.Windows.Forms
+{
+    internal class Application
+    {
+        private static CSSUITheme _theme = new CSSUITheme(CSSFramework.Kubism);
+
+        public static void SetCSSUITheme(CSSUITheme theme)
+        {
+            _theme = theme;
+        }
+
+        public static void Run(Form form)
+        {
+            var sharpJSapp = new WebApplication(_theme);
+            sharpJSapp.Run();
+        }
+    }
+}
