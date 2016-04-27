3ce14dde7483da870145202d2c9dd766585c3728
0xFireball <0xFireball@outlook.com>
2016-04-27 08:43:21 -0700

    update instal script

--- nuget_source/tools/install.ps1
+++ nuget_source/tools/install.ps1
@@ -1,15 +1,1 @@
-param($installPath, $toolsPath, $package, $project)
-
-<#
-Write-Host ("ExaPhaser.WebForms is configuring " + $project.ProjectName)
-
-# Remove all references of object
-$project.Object.References | foreach-object {
-    if ($_.Identity.ToLower().StartsWith("system") -or $_.Identity.ToLower().StartsWith("microsoft")) {
-        Write-Host ("Removing Reference to " + $_.Identity)
-        $_.Remove()
-    }
-}
-#>
-
-Write-Host ("ExaPhaser.WebForms installation was successful! :D")
+Write-Host ("SharpJS - ExaPhaser.WebForms installation was successful! :D")
