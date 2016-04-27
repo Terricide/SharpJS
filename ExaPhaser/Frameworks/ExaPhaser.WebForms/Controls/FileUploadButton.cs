a5e86822029893eb4123e41997293d7e02b4d67c
0xFireball <0xFireball@outlook.com>
2016-04-26 21:18:58 -0700

    format code, add descriptions, support for multiple upload types

--- ExaPhaser/Frameworks/ExaPhaser.WebForms/Controls/FileUploadButton.cs
+++ ExaPhaser/Frameworks/ExaPhaser.WebForms/Controls/FileUploadButton.cs
@@ -7,56 +7,99 @@ using SharpJS.JSLibraries;
 namespace ExaPhaser.WebForms.Controls
 {
     /// <summary>
-    /// A control that represents a file upload button
+    ///     A control that represents a file upload button
     /// </summary>
     public sealed class FileUploadButton : Control
     {
-        public event EventHandler<FileUploadEventArgs> FileOpened;
-
         public FileUploadButton()
         {
             InternalElement = new InputElement
             {
-                Type = "file",
+                Type = "file"
             };
             PerformLayout();
         }
 
+        /// <summary>
+        /// Sets the file encoding if the UploadType is TextFile. The upload button must be reinitialized in order to change this property.
+        /// </summary>
         public FileReaderEncoding FileEncoding { get; set; } = FileReaderEncoding.UTF8;
+
+        /// <summary>
+        /// Sets the type of the file being uploaded, and consequently, the type of parsing. The upload button must be reinitialized in order to change this property.
+        /// </summary>
+        public FileUploadType UploadType { get; set; } = FileUploadType.TextFile;
+
+        public string Filter
+        {
+            set { SetFilter(value); }
+        }
+
+        #region Command
+
+        /// <summary>
+        ///     The command fired when a file is uploaded
+        /// </summary>
+        /// <value>The command.</value>
+        public ICommand Command { get; set; }
+
+        #endregion Command
+
+        public event EventHandler<FileUploadEventArgs> FileOpened;
 
         public override void PerformLayout()
         {
             base.PerformLayout();
-            Action<object, string> fileOpenAction = (jsBlob, textContent) =>
+            Action<object, string, string> fileOpenAction = (jsBlob, textContent, dataUrl) =>
             {
-                var eventArgs = new FileUploadEventArgs(jsBlob, textContent);
+                var eventArgs = new FileUploadEventArgs(jsBlob, textContent, dataUrl);
                 FileOpened?.Invoke(this, eventArgs);
                 Command?.Execute(new ICommandParameter(eventArgs));
             };
 
-            Action<object> changeAction = (object evt) =>
+            Action<object> changeAction = evt =>
             {
                 var input = Verbatim.Expression("evt.target");
                 var file = Verbatim.Expression("$0.files[0]", input);
                 var reader = Verbatim.Expression("new FileReader()");
                 Action onLoadAction = () =>
                 {
-                    object jsBlob = Verbatim.Expression("$0.result", reader);
+                    var jsBlob = Verbatim.Expression("$0.result", reader);
                     //string fileMimeType = (string)Verbatim.Expression("$0.type", file);
-                    string textContent = "";
-                    switch (FileEncoding)
+                    string textContent = null;
+                    string dataUrl = null;
+                    switch (UploadType)
                     {
-                        case FileReaderEncoding.UTF8:
-                            textContent = BufferConverter.ArrayBufferToStringUTF8(jsBlob);
+                        case FileUploadType.TextFile:
+                            switch (FileEncoding)
+                            {
+                                case FileReaderEncoding.UTF8:
+                                    textContent = BufferConverter.ArrayBufferToStringUTF8(jsBlob);
+                                    break;
+
+                                case FileReaderEncoding.UTF16:
+                                    textContent = BufferConverter.ArrayBufferToStringUTF16(jsBlob);
+                                    break;
+                            }
                             break;
-                        case FileReaderEncoding.UTF16:
-                            textContent = BufferConverter.ArrayBufferToStringUTF16(jsBlob);
+                        case FileUploadType.ImageFile:
+                            dataUrl = jsBlob as string;
                             break;
                     }
-                    fileOpenAction(jsBlob, textContent);
+                    fileOpenAction(jsBlob, textContent, dataUrl);
                 };
                 Verbatim.Expression("$0.onload = $1", reader, onLoadAction);
-                Verbatim.Expression("$0.readAsArrayBuffer($1);", reader, file);
+                switch (UploadType)
+                {
+                    case FileUploadType.TextFile:
+                    case FileUploadType.BinaryFile:
+                        Verbatim.Expression("$0.readAsArrayBuffer($1);", reader, file);
+                        break;
+
+                    case FileUploadType.ImageFile:
+                        Verbatim.Expression("$0.readAsDataURL($1);", reader, file);
+                        break;
+                }
             };
             InternalJQElement.BindEventListener("change", changeAction);
         }
@@ -66,20 +109,20 @@ namespace ExaPhaser.WebForms.Controls
             // Process the filter list to convert the syntax from C# standard filter syntax to HTML5:
             // Windows application filter syntax: Image Files (*.bmp, *.jpg)|*.bmp;*.jpg|All Files (*.*)|*.*
             // HTML5 filter syntax: .gif, .jpg, .png, .doc
-            string[] fileTypes = filter.Split('|');
-            List<string> htmlFileTypes = new List<string>();
+            var fileTypes = filter.Split('|');
+            var htmlFileTypes = new List<string>();
             if (fileTypes.Length == 1)
             {
                 htmlFileTypes.Add(fileTypes[0]);
             }
             else
             {
-                for (int i = 1; i < fileTypes.Length; i += 2)
+                for (var i = 1; i < fileTypes.Length; i += 2)
                 {
                     htmlFileTypes.Add(fileTypes[i]);
                 }
             }
-            string htmlFilter = String.Join(",", htmlFileTypes).Replace("*", "").Replace(";", ",");
+            var htmlFilter = string.Join(",", htmlFileTypes).Replace("*", "").Replace(";", ",");
 
             // Apply the filter:
             if (!string.IsNullOrWhiteSpace(htmlFilter))
@@ -91,42 +134,48 @@ namespace ExaPhaser.WebForms.Controls
                 Verbatim.Expression(@"$0.accept = """"", InternalElement.DOMRepresentation);
             }
         }
+    }
 
-        public string Filter
-        {
-            set { SetFilter(value); }
-        }
-
-        #region Command
-
-        /// <summary>
-        /// The command fired when a file is uploaded
-        /// </summary>
-        /// <value>The command.</value>
-        public ICommand Command { get; set; }
-
-        #endregion Command
+    public enum FileUploadType
+    {
+        TextFile,
+        ImageFile,
+        BinaryFile
     }
 
     public enum FileReaderEncoding
     {
         UTF8,
-        UTF16,
+        UTF16
     }
 
+    /// <summary>
+    /// Contains the data from a file upload. If the upload and resulting parse succeeded, its properties contain the results of the uploaded file depending on the UploadType.
+    /// </summary>
     public class FileUploadEventArgs : EventArgs
     {
         /// <summary>
-        /// Only available if file is of type Plain Text.
+        ///     If the UploadType of the UploadButton was ImageFile, this contains the data URL representation of the file
+        ///     uploaded. Otherwise, it is null.
+        /// </summary>
+        public readonly string DataURL;
+
+        /// <summary>
+        ///     This object contains the JavaScript Blob object
+        /// </summary>
+        public readonly object JSBlob;
+
+        /// <summary>
+        ///     If the UploadType of the UploadButton was TextFile, this contains the text representation of the file uploaded.
+        ///     Otherwise, it is null.
         /// </summary>
         public readonly string PlainTextContent;
 
-        public readonly object JavaScriptBlob;
-
-        public FileUploadEventArgs(object javaScriptBlob, string plainTextContent)
+        public FileUploadEventArgs(object jsBlob, string plainTextContent, string dataUrl)
         {
-            this.JavaScriptBlob = javaScriptBlob;
-            this.PlainTextContent = plainTextContent;
+            JSBlob = jsBlob;
+            PlainTextContent = plainTextContent;
+            DataURL = dataUrl;
         }
     }
 }
