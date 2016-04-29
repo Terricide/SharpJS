3ae350a4df4a8120c11dcd2292cd8bced0ca7ff4
Dave Ho <daveho8888@gmail.com>
2016-04-29 12:23:31 -0700

    Update rich text box
    Fully implemented

--- ExaPhaser/Extensions/ExaPhaser.WebForms.Extensions.WinFormsCompat/Windows/Forms/RichTextBox.cs
+++ ExaPhaser/Extensions/ExaPhaser.WebForms.Extensions.WinFormsCompat/Windows/Forms/RichTextBox.cs
@@ -1,1 +1,10 @@
-
+namespace System.Windows.Forms
+{
+    public class RichTextBox : TextControl
+    {
+        public RichTextBox()
+        {
+            WebFormsControl = new ExaPhaser.WebForms.Controls.TextArea();
+        }
+    }
+}
