37debc1ffaef7af3aa171ee54db7afe153af988f
0xFireball <0xFireball@outlook.com>
2016-09-17 17:09:57 -0700

    update versioning and add upcoming NeptuniumKit library

--- ExaPhaser/Frameworks/IridiumIon.NeptuniumKit/Properties/AssemblyInfo.cs
+++ ExaPhaser/Frameworks/IridiumIon.NeptuniumKit/Properties/AssemblyInfo.cs
@@ -4,25 +4,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-[assembly: AssemblyTitle("ExaPhaser.WebForms")]
-[assembly: AssemblyDescription("ExaPhaser WebForms GUI Framework")]
+[assembly: AssemblyTitle("IridiumIon.NeptuniumKit")]
+[assembly: AssemblyDescription("NeptuniumKit UI Framework for SharpJS")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ExaPhaser Industries")]
-[assembly: AssemblyProduct("ExaPhaser.WebForms")]
-[assembly: AssemblyCopyright("Copyright © 2016 ExaPhaser Industries")]
+[assembly: AssemblyCompany("IridiumIon Software")]
+[assembly: AssemblyProduct("IridiumIon NeptuniumKit UI Framework for SharpJS")]
+[assembly: AssemblyCopyright("Copyright ©  2016 0xFireball, IridiumIon Software. All Rights Reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("f8ab49de-7804-49e4-b0c3-34b078ffc891")]
+[assembly: Guid("e3a4e99c-788f-4aec-8660-7e4d475ec360")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,6 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.6.*")]
-[assembly: AssemblyFileVersion("1.6")]
+[assembly: AssemblyVersion("0.2.*")]
+[assembly: AssemblyFileVersion("0.2")]
