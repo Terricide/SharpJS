d3d88a9906be5f1e6f8380d0874b74b20a21346f
0xFireball <0xFireball@outlook.com>
2016-09-19 23:50:36 -0700

    code cleanup and note

--- ExaPhaser/Frameworks/IridiumIon.NeptuniumKit/NKBootstrapper.cs
+++ ExaPhaser/Frameworks/IridiumIon.NeptuniumKit/NKBootstrapper.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace IridiumIon.NeptuniumKit
+﻿namespace IridiumIon.NeptuniumKit
 {
     public static class NKBootstrapper
     {
         public static void StartApplication(NKApplication app)
         {
-
+            //TODO: Initialize DOM host object with jQuery
         }
     }
 }
